753a85407661f12462cdd002c4039c682e85cc85
Gimsongsam <gimsong83@gmail.com>
2021-09-12 20:25:03 +0900

    신규 아이디 추천 문제 풀이 수정

--- implementation/programmers_신규 아이디 추천.py
+++ implementation/programmers_신규 아이디 추천.py
@@ -2,13 +2,15 @@ from collections import deque
 
 def solution(new_id):
     # 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-    new_id.lower()
+    new_id = new_id.lower()
 
     # 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
     token = '~!@#$%^&*()=+[{]}:?,<>/'
     for remove in token:
         new_id = new_id.replace(remove, "")
         # print(remove)
+
+    print(new_id)
 
     # 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
     # 첫 번째 인덱스는 이전 값이 없으므로 이전값과 비교할 수 없으므로 .이 아니라면 추가해주기
@@ -28,6 +30,8 @@ def solution(new_id):
         # .이라면 continue
         else:
             continue
+    
+    print(id_arr)
 
     # 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
     if id_arr[0] == '.' or id_arr[-1] == '.':
@@ -42,7 +46,7 @@ def solution(new_id):
 
     new_id = (''.join(id_arr))
 
-    
+    print(new_id)
     
     # 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
     if len(new_id) == 0:
@@ -53,7 +57,9 @@ def solution(new_id):
     if len(new_id) >= 16:
         new_id = new_id[:15]
     if new_id[-1] == '.':
-        new_id = new_id[-1:]
+        new_id = new_id[:-1]
+
+    print(new_id)
     
     # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
     if len(new_id) <= 2:
@@ -67,4 +73,4 @@ def solution(new_id):
     answer = new_id
     return answer
 
-print(solution("abcdefghijklmn.p"))
+print("최종결과값: ", solution("...!@BaT#*..y.abcdefghijklm"))
