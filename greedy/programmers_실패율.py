ca0f9d2ff5aad22a4f54c3bdbb7379a66fbfe2f6
Gimsongsam <gimsong83@gmail.com>
2021-10-19 16:19:20 +0900

    programmers_실패율 update

--- greedy/programmers_실패율.py
+++ greedy/programmers_실패율.py
@@ -3,17 +3,30 @@
 # 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 
 
 def solution(N, stages):
-    answer = []
+    
     user = len(stages)
 
-    for i in range(5):
-        failure = stages.count(i+1) / (user)
+    data = {}
+    for i in range(N):
+        # print('탐색하고있는 스테이지:', i+1)
 
-        # stages.count(i+1) - 
-        
-        print(stages.count(i+1), '/', (stages.count(i+1) - user), '=', failure)
+        # 실패율 계산
+        if user == 0: # 0으로 나눌 수 없기 때문에 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0으로 정의
+            failure = 0
+        else:
+            failure = stages.count(i+1) / (user)
+        # print(stages.count(i+1), '/', user, '=', failure)
+        data[i+1] = failure
+        print(data)
 
-    return answer
+        # 다음 스테이지 실패율 계산을 위해 실패한 참가자 수 만큼 user 수를 빼기
+        user -= stages.count(i+1)
+        # print('남은 유저 수: ', user)
 
-solution(5, [2, 1, 2, 6, 2, 4, 3, 3])
+    answer = sorted(data.items(), reverse=True, key=lambda item: item[1])
+    # print('최종 데이터: ', answer)
+
+    return [i[0] for i in answer]
+
+print(solution(5, [2, 2, 2, 2, 2]))
 
