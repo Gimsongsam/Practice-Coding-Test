d3262a2c3e6e9bef9777d74b9c045aa62c92c8f2
Gimsongsam <gimsong83@gmail.com>
2021-11-01 16:51:00 +0900

    programmers_오픈채팅방 update

---
+++ implementation/programmers_오픈채팅방.py
@@ -0,0 +1,36 @@
+case = ['Enter', 'Leave', 'Change']
+
+def change(info, user):
+
+    # Change
+    if case[2] == info[0]:
+        user[info[1]] = info[2]
+    # Enter
+    elif case[0] == info[0]:
+        user[info[1]] = info[2]
+    # Leave
+    else:
+        return
+
+def solution(record):
+    answer = []
+    
+    user = {}
+    info = []
+    for i, j in enumerate(record):
+        info.append(j.split())
+        change(info[i], user)
+    
+    for i in info:
+        # Change
+        if case[2] == i[0]:
+            continue
+        # Enter
+        elif case[0] == i[0]:
+            answer.append('{0}님이 들어왔습니다.'.format(user[i[1]]))
+        # Leave
+        else:
+            answer.append('{0}님이 나갔습니다.'.format(user[i[1]]))
+    
+    return answer
+solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"])
